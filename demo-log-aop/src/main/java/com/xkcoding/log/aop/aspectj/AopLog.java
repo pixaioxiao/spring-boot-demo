68d4378a8232806c65283940ed99fc1cdee6fc7b
pixiaoxiao <3014774743@qq.com>
2024-09-20 14:55:52 +0800

    fix(AopLog)优化日志切面处理以排除HTTP请求和响应对象在日志切面的参数处理逻辑中，HttpServletRequest和HttpServletResponse对象现被明确排除，以防止序列化问题导致的异常。此更改通过在getNameAndValue方法中跳过这两个特定类型的参数，保留了功能的完整性并解决了潜在的错误。 ```

--- demo-log-aop/src/main/java/com/xkcoding/log/aop/aspectj/AopLog.java
+++ demo-log-aop/src/main/java/com/xkcoding/log/aop/aspectj/AopLog.java
@@ -20,6 +20,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Collections;
@@ -39,140 +40,146 @@ import java.util.Objects;
 @Component
 @Slf4j
 public class AopLog {
-    /**
-     * 切入点
-     */
-    @Pointcut("execution(public * com.xkcoding.log.aop.controller.*Controller.*(..))")
-    public void log() {
+  /**
+   * 切入点
+   */
+  @Pointcut("execution(public * com.xkcoding.log.aop.controller.*Controller.*(..))")
+  public void log() {
 
+  }
+
+  /**
+   * 环绕操作
+   *
+   * @param point 切入点
+   * @return 原方法返回值
+   * @throws Throwable 异常信息
+   */
+  @Around("log()")
+  public Object aroundLog(ProceedingJoinPoint point) throws Throwable {
+
+    // 开始打印请求日志
+    ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+    HttpServletRequest request = Objects.requireNonNull(attributes).getRequest();
+
+    // 打印请求相关参数
+    long startTime = System.currentTimeMillis();
+    Object result = point.proceed();
+    String header = request.getHeader("User-Agent");
+    UserAgent userAgent = UserAgent.parseUserAgentString(header);
+
+    final Log l = Log.builder()
+      .threadId(Long.toString(Thread.currentThread().getId()))
+      .threadName(Thread.currentThread().getName())
+      .ip(getIp(request))
+      .url(request.getRequestURL().toString())
+      .classMethod(String.format("%s.%s", point.getSignature().getDeclaringTypeName(),
+        point.getSignature().getName()))
+      .httpMethod(request.getMethod())
+      .requestParams(getNameAndValue(point))
+      .result(result)
+      .timeCost(System.currentTimeMillis() - startTime)
+      .userAgent(header)
+      .browser(userAgent.getBrowser().toString())
+      .os(userAgent.getOperatingSystem().toString()).build();
+
+    log.info("Request Log Info : {}", JSONUtil.toJsonStr(l));
+
+    return result;
+  }
+
+  /**
+   * 获取方法参数名和参数值
+   *
+   * @param joinPoint
+   * @return
+   */
+  private Map<String, Object> getNameAndValue(ProceedingJoinPoint joinPoint) {
+
+    final Signature signature = joinPoint.getSignature();
+    MethodSignature methodSignature = (MethodSignature) signature;
+    final String[] names = methodSignature.getParameterNames();
+    final Object[] args = joinPoint.getArgs();
+
+    if (ArrayUtil.isEmpty(names) || ArrayUtil.isEmpty(args)) {
+      return Collections.emptyMap();
     }
-
-    /**
-     * 环绕操作
-     *
-     * @param point 切入点
-     * @return 原方法返回值
-     * @throws Throwable 异常信息
-     */
-    @Around("log()")
-    public Object aroundLog(ProceedingJoinPoint point) throws Throwable {
-
-        // 开始打印请求日志
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = Objects.requireNonNull(attributes).getRequest();
-
-        // 打印请求相关参数
-        long startTime = System.currentTimeMillis();
-        Object result = point.proceed();
-        String header = request.getHeader("User-Agent");
-        UserAgent userAgent = UserAgent.parseUserAgentString(header);
-
-        final Log l = Log.builder()
-            .threadId(Long.toString(Thread.currentThread().getId()))
-            .threadName(Thread.currentThread().getName())
-            .ip(getIp(request))
-            .url(request.getRequestURL().toString())
-            .classMethod(String.format("%s.%s", point.getSignature().getDeclaringTypeName(),
-                point.getSignature().getName()))
-            .httpMethod(request.getMethod())
-            .requestParams(getNameAndValue(point))
-            .result(result)
-            .timeCost(System.currentTimeMillis() - startTime)
-            .userAgent(header)
-            .browser(userAgent.getBrowser().toString())
-            .os(userAgent.getOperatingSystem().toString()).build();
-
-        log.info("Request Log Info : {}", JSONUtil.toJsonStr(l));
-
-        return result;
+    if (names.length != args.length) {
+      log.warn("{}方法参数名和参数值数量不一致", methodSignature.getName());
+      return Collections.emptyMap();
     }
+    Map<String, Object> map = Maps.newHashMap();
+    for (int i = 0; i < names.length; i++) {
 
-    /**
-     *  获取方法参数名和参数值
-     * @param joinPoint
-     * @return
-     */
-    private Map<String, Object> getNameAndValue(ProceedingJoinPoint joinPoint) {
-
-        final Signature signature = joinPoint.getSignature();
-        MethodSignature methodSignature = (MethodSignature) signature;
-        final String[] names = methodSignature.getParameterNames();
-        final Object[] args = joinPoint.getArgs();
-
-        if (ArrayUtil.isEmpty(names) || ArrayUtil.isEmpty(args)) {
-            return Collections.emptyMap();
-        }
-        if (names.length != args.length) {
-            log.warn("{}方法参数名和参数值数量不一致", methodSignature.getName());
-            return Collections.emptyMap();
-        }
-        Map<String, Object> map = Maps.newHashMap();
-        for (int i = 0; i < names.length; i++) {
-            map.put(names[i], args[i]);
-        }
-        return map;
+      // 剔除 HttpServletRequest request  httpServletResponse response,因为后边打印日,这个两个参数没有序列化会导致异常
+      if (args[i] instanceof HttpServletRequest || args[i] instanceof HttpServletResponse) {
+        continue;
+      }
+      map.put(names[i], args[i]);
     }
+    return map;
+  }
 
-    private static final String UNKNOWN = "unknown";
+  private static final String UNKNOWN = "unknown";
 
-    /**
-     * 获取ip地址
-     */
-    public static String getIp(HttpServletRequest request) {
-        String ip = request.getHeader("x-forwarded-for");
-        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-        }
-        String comma = ",";
-        String localhost = "127.0.0.1";
-        if (ip.contains(comma)) {
-            ip = ip.split(",")[0];
-        }
-        if (localhost.equals(ip)) {
-            // 获取本机真正的ip地址
-            try {
-                ip = InetAddress.getLocalHost().getHostAddress();
-            } catch (UnknownHostException e) {
-                log.error(e.getMessage(), e);
-            }
-        }
-        return ip;
+  /**
+   * 获取ip地址
+   */
+  public static String getIp(HttpServletRequest request) {
+    String ip = request.getHeader("x-forwarded-for");
+    if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
+      ip = request.getHeader("Proxy-Client-IP");
     }
-
-    @Data
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    static class Log {
-        // 线程id
-        private String threadId;
-        // 线程名称
-        private String threadName;
-        // ip
-        private String ip;
-        // url
-        private String url;
-        // http方法 GET POST PUT DELETE PATCH
-        private String httpMethod;
-        // 类方法
-        private String classMethod;
-        // 请求参数
-        private Object requestParams;
-        // 返回参数
-        private Object result;
-        // 接口耗时
-        private Long timeCost;
-        // 操作系统
-        private String os;
-        // 浏览器
-        private String browser;
-        // user-agent
-        private String userAgent;
+    if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
+      ip = request.getHeader("WL-Proxy-Client-IP");
     }
+    if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
+      ip = request.getRemoteAddr();
+    }
+    String comma = ",";
+    String localhost = "127.0.0.1";
+    if (ip.contains(comma)) {
+      ip = ip.split(",")[0];
+    }
+    if (localhost.equals(ip)) {
+      // 获取本机真正的ip地址
+      try {
+        ip = InetAddress.getLocalHost().getHostAddress();
+      } catch (UnknownHostException e) {
+        log.error(e.getMessage(), e);
+      }
+    }
+    return ip;
+  }
+
+  @Data
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  static class Log {
+    // 线程id
+    private String threadId;
+    // 线程名称
+    private String threadName;
+    // ip
+    private String ip;
+    // url
+    private String url;
+    // http方法 GET POST PUT DELETE PATCH
+    private String httpMethod;
+    // 类方法
+    private String classMethod;
+    // 请求参数
+    private Object requestParams;
+    // 返回参数
+    private Object result;
+    // 接口耗时
+    private Long timeCost;
+    // 操作系统
+    private String os;
+    // 浏览器
+    private String browser;
+    // user-agent
+    private String userAgent;
+  }
 }
